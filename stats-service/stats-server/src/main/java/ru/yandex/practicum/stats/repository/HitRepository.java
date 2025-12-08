0d3a423d384c410f683a75216c6087cf08252ae2
Daniil Sebiakin <sebyackindaniil@ya.ru>
2025-12-08 14:10:51 +0300

    fix

--- stats-service/stats-server/src/main/java/ru/yandex/practicum/stats/repository/HitRepository.java
+++ stats-service/stats-server/src/main/java/ru/yandex/practicum/stats/repository/HitRepository.java
@@ -12,19 +12,19 @@ import java.util.List;
 @Repository
 public interface HitRepository extends JpaRepository<Hit, Integer> {
 
-    @Query("SELECT new ru.practicum.ewm.dto.StatsDto(a.name, h.uri, COUNT (h.id)) " +
+    @Query("SELECT new ru.yandex.practicum.dto.StatsDto(a.name, h.uri, COUNT (h.id)) " +
             "FROM Hit h JOIN App a ON a.id=h.app.id " +
             "WHERE h.timestamp BETWEEN :startDateTime AND :endDateTime " +
             "GROUP BY  h.uri, a.name " +
             "ORDER BY COUNT(h.id) DESC")
     List<StatsDto> findAllHits(LocalDateTime startDateTime, LocalDateTime endDateTime);
 
-    @Query("SELECT new ru.practicum.ewm.dto.StatsDto(name, uri)" +
+    @Query("SELECT new ru.yandex.practicum.dto.StatsDto(name, uri)" +
             "FROM (select distinct h.ip as ip, a.name as name, h.uri as uri from Hit h JOIN App a ON a.id=h.app.id " +
             "WHERE h.timestamp BETWEEN :startDateTime AND :endDateTime)")
     List<StatsDto> findAllUniqueHits(LocalDateTime startDateTime, LocalDateTime endDateTime);
 
-    @Query("SELECT new ru.practicum.ewm.dto.StatsDto(a.name, h.uri, count (h.id))" +
+    @Query("SELECT new ru.yandex.practicum.dto.StatsDto(a.name, h.uri, count (h.id))" +
             "FROM  Hit h JOIN App a ON a.id=h.app.id " +
             "WHERE (h.timestamp BETWEEN :startDateTime AND :endDateTime) AND (h.uri IN (:uris))" +
             "GROUP BY h.uri, a.name " +
@@ -32,7 +32,7 @@ public interface HitRepository extends JpaRepository<Hit, Integer> {
     List<StatsDto> findHitsByUris(LocalDateTime startDateTime, LocalDateTime endDateTime, List<String> uris);
 
 
-    @Query("SELECT new ru.practicum.ewm.dto.StatsDto(name, uri)" +
+    @Query("SELECT new ru.yandex.practicum.dto.StatsDto(name, uri)" +
             "FROM (select distinct h.ip as ip, a.name as name, h.uri as uri from Hit h JOIN App a ON a.id=h.app.id " +
             "WHERE h.timestamp BETWEEN :startDateTime AND :endDateTime  AND (h.uri IN (:uris)))")
     List<StatsDto> findUniqueHitsByUris(LocalDateTime startDateTime, LocalDateTime endDateTime, List<String> uris);
