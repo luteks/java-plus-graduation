7a13b21ce8bfc5fc8170d768a00d6d48c7bbe3f4
Daniil Sebiakin <sebyackindaniil@ya.ru>
2026-01-20 19:51:34 +0300

    fix

--- stats-service/stats-dto/src/main/java/ru/yandex/practicum/dto/HitDto.java
+++ stats-service/stats-dto/src/main/java/ru/yandex/practicum/dto/HitDto.java
@@ -21,4 +21,10 @@ public class HitDto {
     private String ip;
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", shape = JsonFormat.Shape.STRING)
     private LocalDateTime timestamp;
+    public HitDto(String ip, String app, String uri, LocalDateTime timestamp) {
+        this.ip = ip;
+        this.app = app;
+        this.uri = uri;
+        this.timestamp = timestamp;
+    }
 }
