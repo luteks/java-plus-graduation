595893b698ecb4e27c2f4fe49cd2a793e24d8d8c
Daniil Sebiakin <sebyackindaniil@ya.ru>
2026-01-21 03:14:35 +0300

    fix

--- core/event-service/src/main/java/ru/yandex/practicum/controller/EventPrivateController.java
+++ core/event-service/src/main/java/ru/yandex/practicum/controller/EventPrivateController.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
@@ -8,59 +7,50 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.client.StatsClient;
-import ru.yandex.practicum.dto.HitDto;
 import ru.yandex.practicum.dto.event.CreateNewEventDto;
 import ru.yandex.practicum.dto.event.EventDto;
 import ru.yandex.practicum.dto.event.EventShortDto;
 import ru.yandex.practicum.dto.event.UpdateEventUserRequest;
 import ru.yandex.practicum.service.EventService;
-import ru.yandex.practicum.service.StatsHitAsyncService;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
 @RequestMapping(path = "/users")
 public class EventPrivateController {
-    private final EventService eventService;
-    private final StatsClient statClient;
 
-    public EventPrivateController(EventService eventService, StatsClient statClient) {
+    private final EventService eventService;
+
+    public EventPrivateController(EventService eventService) {
         this.eventService = eventService;
-        this.statClient = statClient;
     }
 
     @GetMapping("/{userId}/events")
-    public List<EventShortDto> getUserEvents(@PathVariable Long userId,
-                                             @PositiveOrZero @RequestParam(value = "from", defaultValue = "0") int from,
-                                             @Positive @RequestParam(value = "size", defaultValue = "10") int size,
-                                             HttpServletRequest request) {
-        Pageable paging = PageRequest.of(from, size);
-        statClient.create(new HitDto(request.getRemoteAddr(), "ewm-main-service", "/events", LocalDateTime.now()));
+    public List<EventShortDto> getUserEvents(
+            @PathVariable Long userId,
+            @PositiveOrZero @RequestParam(value = "from", defaultValue = "0") int from,
+            @Positive @RequestParam(value = "size", defaultValue = "10") int size) {
 
+        Pageable paging = PageRequest.of(from, size);
         return eventService.getByUserId(userId, paging);
     }
 
     @PostMapping("/{userId}/events")
     @ResponseStatus(HttpStatus.CREATED)
     public EventDto createEvent(@RequestBody @Valid CreateNewEventDto createNewEventDto, @PathVariable Long userId) {
-
         return eventService.create(createNewEventDto, userId);
     }
 
     @GetMapping("/{userId}/events/{eventId}")
-    public EventDto getUserEvents(@PathVariable Long userId, @PathVariable Long eventId, HttpServletRequest request) {
-        statClient.create(new HitDto(request.getRemoteAddr(), "ewm-main-service", "/events", LocalDateTime.now()));
-
+    public EventDto getUserEvents(@PathVariable Long userId, @PathVariable Long eventId) {
         return eventService.getEventByUserId(userId, eventId);
     }
 
     @PatchMapping("/{userId}/events/{eventId}")
-    public EventDto updateEvent(@RequestBody @Valid UpdateEventUserRequest eventDto,
-                                @PathVariable Long userId, @PathVariable Long eventId, HttpServletRequest request) {
-        statClient.create(new HitDto(request.getRemoteAddr(), "ewm-main-service", "/events/" + eventId,
-                LocalDateTime.now()));
+    public EventDto updateEvent(
+            @RequestBody @Valid UpdateEventUserRequest eventDto,
+            @PathVariable Long userId,
+            @PathVariable Long eventId) {
 
         return eventService.updateByUser(eventDto, userId, eventId);
     }
--- core/event-service/src/main/java/ru/yandex/practicum/controller/EventPublicController.java
+++ core/event-service/src/main/java/ru/yandex/practicum/controller/EventPublicController.java
@@ -6,11 +6,10 @@ import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.client.StatsClient;
-import ru.yandex.practicum.dto.HitDto;
 import ru.yandex.practicum.dto.event.EventDto;
 import ru.yandex.practicum.dto.event.EventShortDto;
 import ru.yandex.practicum.service.EventService;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -19,39 +18,30 @@ import java.util.List;
 public class EventPublicController {
 
     private final EventService eventService;
-    private final StatsClient statClient;
 
-
-    public EventPublicController(EventService eventService, StatsClient statClient) {
+    public EventPublicController(EventService eventService) {
         this.eventService = eventService;
-        this.statClient = statClient;
     }
 
     @GetMapping("/{id}")
-    public EventDto getEvent(@PathVariable Long id,
-                             HttpServletRequest request) {
-        statClient.create(new HitDto(request.getRemoteAddr(), "ewm-main-service", request.getRequestURI(),
-                LocalDateTime.now()));
-
-        return eventService.getById(id,request);
+    public EventDto getEvent(@PathVariable Long id, HttpServletRequest request) {
+        return eventService.getById(id, request);
     }
 
     @GetMapping
-    public List<EventShortDto> getAllEvents(@RequestParam(value = "text", required = false) @Size(min = 3) String text,
-                                            @RequestParam(value = "categories", required = false) List<Long> categories,
-                                            @RequestParam(value = "paid", required = false) Boolean paid,
-                                            @RequestParam(value = "rangeStart", required = false)
-                                            @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime rangeStart,
-                                            @RequestParam(value = "rangeEnd", required = false)
-                                            @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime rangeEnd,
-                                            @RequestParam(value = "onlyAvailable", defaultValue = "false")
-                                            boolean onlyAvailable,
-                                            @RequestParam(value = "sort", required = false) String sort,
-                                            @PositiveOrZero @RequestParam(value = "from", defaultValue = "0") int from,
-                                            @Positive @RequestParam(value = "size", defaultValue = "10") int size,
-                                            HttpServletRequest request) {
-        statClient.create(new HitDto(request.getRemoteAddr(), "ewm-main-service", request.getRequestURI(),
-                LocalDateTime.now()));
+    public List<EventShortDto> getAllEvents(
+            @RequestParam(value = "text", required = false) @Size(min = 3) String text,
+            @RequestParam(value = "categories", required = false) List<Long> categories,
+            @RequestParam(value = "paid", required = false) Boolean paid,
+            @RequestParam(value = "rangeStart", required = false)
+            @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime rangeStart,
+            @RequestParam(value = "rangeEnd", required = false)
+            @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime rangeEnd,
+            @RequestParam(value = "onlyAvailable", defaultValue = "false") boolean onlyAvailable,
+            @RequestParam(value = "sort", required = false) String sort,
+            @PositiveOrZero @RequestParam(value = "from", defaultValue = "0") int from,
+            @Positive @RequestParam(value = "size", defaultValue = "10") int size,
+            HttpServletRequest request) {
 
         return eventService.getAllShort(text, categories, paid, rangeStart, rangeEnd, onlyAvailable, sort, from, size);
     }
