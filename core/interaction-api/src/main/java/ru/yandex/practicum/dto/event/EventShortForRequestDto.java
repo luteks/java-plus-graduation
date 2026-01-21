4f3ccdf2918d2bcf0758314166256260a3ae2f37
Daniil Sebiakin <sebyackindaniil@ya.ru>
2026-01-21 15:23:14 +0300

    fix

--- core/interaction-api/src/main/java/ru/yandex/practicum/dto/event/EventShortForRequestDto.java
+++ core/interaction-api/src/main/java/ru/yandex/practicum/dto/event/EventShortForRequestDto.java
@@ -10,17 +10,8 @@ import java.time.Instant;
 @Builder
 public class EventShortForRequestDto {
     private Long id;
-    private Initiator initiator;
+    private Long ownerId;
     private Instant publishedOn;
     private Boolean isModerated;
     private Integer participantLimit;
-
-    @Getter @Setter
-    public static class Initiator {
-        private Long id;
-    }
-
-    public Long getOwnerId() {
-        return initiator != null ? initiator.id : null;
-    }
 }
--- core/interaction-api/src/main/java/ru/yandex/practicum/feign/event/EventClient.java
+++ core/interaction-api/src/main/java/ru/yandex/practicum/feign/event/EventClient.java
@@ -5,9 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import ru.yandex.practicum.dto.event.EventShortForRequestDto;
 
-@FeignClient(name = "EVENT-SERVICE", path = "/internal/events")
+@FeignClient(name = "EVENT-SERVICE")
 public interface EventClient {
-
-    @GetMapping("/{eventId}")
+    @GetMapping("/internal/events/{eventId}")
     EventShortForRequestDto getById(@PathVariable Long eventId);
 }
