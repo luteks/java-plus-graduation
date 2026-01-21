c50bee73da425ab423cf9d085c0ac80150fe151d
Daniil Sebiakin <sebyackindaniil@ya.ru>
2026-01-21 14:01:58 +0300

    Revert "fix"
    This reverts commit 0734ffaad4e32b43ea1483bde30d03d748bc7ac1.

--- core/event-service/src/main/java/ru/yandex/practicum/model/Event.java
+++ core/event-service/src/main/java/ru/yandex/practicum/model/Event.java
@@ -61,6 +61,7 @@ public class Event {
     @Column(nullable = false)
     private EventState state;
 
+    // НОВОЕ ПОЛЕ
     @Column(name = "confirmed_requests", nullable = false)
     private Long confirmedRequests = 0L;
 }
